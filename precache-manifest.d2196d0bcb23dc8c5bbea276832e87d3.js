29cc3967dc9d495013b39dd6a7c7fb6eed450901
root <root@localhost.localdomain>
2018-11-17 06:38:40 -0500

    Updates

---
+++ precache-manifest.d2196d0bcb23dc8c5bbea276832e87d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "a6dc00c6e6cc5295fb35",
+    "url": "/movie_app/static/js/main.a6dc00c6.chunk.js"
+  },
+  {
+    "revision": "839fb13c83a50e78abfa",
+    "url": "/movie_app/static/js/1.839fb13c.chunk.js"
+  },
+  {
+    "revision": "a6dc00c6e6cc5295fb35",
+    "url": "/movie_app/static/css/main.798a1f9d.chunk.css"
+  },
+  {
+    "revision": "a23cffcdcd6edde05196d1ae544620ca",
+    "url": "/movie_app/index.html"
+  }
+];
